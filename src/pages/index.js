b409eebee3183b56a70171bf92e082244717b275
maksims-v <spread2010@inbox.ru>
2023-09-22 12:16:58 +0300

    remove serverside function

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,17 +11,19 @@ import { setHeaderData } from '@/state/headerSlice';
 import { setFooterData } from '@/state/footerSlice';
 import { useEffect } from 'react';
 
-const Home = ({
-  headerData,
-  footerData,
-  bannerData,
-  newProductsData,
-  clearenceData,
-  sectionCategoryData,
-  secondBannerData,
-  sectionPopularCategoryData,
-  sectionBrandData,
-}) => {
+const Home = (
+  {
+    // headerData,
+    // footerData,
+    // bannerData,
+    // newProductsData,
+    // clearenceData,
+    // sectionCategoryData,
+    // secondBannerData,
+    // sectionPopularCategoryData,
+    // sectionBrandData,
+  },
+) => {
   const dispatch = useDispatch();
 
   // useEffect(() => {
@@ -31,10 +33,10 @@ const Home = ({
 
   return (
     <>
-      <SectionBanner bannerData={bannerData} />
+      {/* <SectionBanner bannerData={bannerData} />
       <PopularCategorySection sectionPopularCategoryData={sectionPopularCategoryData} />
       <SectionBrands sectionBrandData={sectionBrandData} />
-      {/* <NewArrivalsSlider newProductsData={newProductsData} />
+      <NewArrivalsSlider newProductsData={newProductsData} />
       <SecondSectionBanner secondBannerData={secondBannerData} />
       <SectionCategory
         sectionCategoryData={
@@ -48,93 +50,93 @@ const Home = ({
 
 export default Home;
 
-export async function getServerSideProps() {
-  try {
-    const query = qs.stringify({
-      filters: {
-        clearance: true,
-      },
-      populate: { image: true },
-    });
+// export async function getServerSideProps() {
+//   try {
+//     const query = qs.stringify({
+//       filters: {
+//         clearance: true,
+//       },
+//       populate: { image: true },
+//     });
 
-    const sectionCategoryQuery = qs.stringify({
-      populate: {
-        category: {
-          populate: { image: true },
-        },
-      },
-    });
+//     const sectionCategoryQuery = qs.stringify({
+//       populate: {
+//         category: {
+//           populate: { image: true },
+//         },
+//       },
+//     });
 
-    const sectionPopularCategoryQuery = qs.stringify({
-      populate: {
-        popularCategeory: {
-          populate: { image: true },
-        },
-      },
-    });
+//     const sectionPopularCategoryQuery = qs.stringify({
+//       populate: {
+//         popularCategeory: {
+//           populate: { image: true },
+//         },
+//       },
+//     });
 
-    const sectionBrandQuery = qs.stringify({
-      populate: {
-        brandSection: {
-          populate: { image: true, items: { populate: { image: true } } },
-        },
-      },
-    });
+//     const sectionBrandQuery = qs.stringify({
+//       populate: {
+//         brandSection: {
+//           populate: { image: true, items: { populate: { image: true } } },
+//         },
+//       },
+//     });
 
-    // const footerResponse = await fetch(`${process.env.API_URL}/api/layout-footers`);
-    // const headerResponse = await fetch(`${process.env.API_URL}/api/layout-header`);
-    const bannerResponse = await fetch(`${process.env.API_URL}/api/section-banners?populate=*`);
-    // const secondBannerResponse = await fetch(
-    //   `${process.env.API_URL}/api/second-section-banners?populate=*`,
-    // );
-    const popularCategoryResponse = await fetch(
-      `${process.env.API_URL}/api/section-popular-categories?${sectionPopularCategoryQuery}`,
-    );
-    // const newProductsResponse = await fetch(`${process.env.API_URL}/api/products/newarrivals`);
-    // const clearenceResponse = await fetch(`${process.env.API_URL}/api/products?${query}`);
-    // const sectionCategoryResponse = await fetch(
-    //   `${process.env.API_URL}/api/section-categories?${sectionCategoryQuery}`,
-    // );
+//     // const footerResponse = await fetch(`${process.env.API_URL}/api/layout-footers`);
+//     // const headerResponse = await fetch(`${process.env.API_URL}/api/layout-header`);
+//     const bannerResponse = await fetch(`${process.env.API_URL}/api/section-banners?populate=*`);
+//     // const secondBannerResponse = await fetch(
+//     //   `${process.env.API_URL}/api/second-section-banners?populate=*`,
+//     // );
+//     const popularCategoryResponse = await fetch(
+//       `${process.env.API_URL}/api/section-popular-categories?${sectionPopularCategoryQuery}`,
+//     );
+//     // const newProductsResponse = await fetch(`${process.env.API_URL}/api/products/newarrivals`);
+//     // const clearenceResponse = await fetch(`${process.env.API_URL}/api/products?${query}`);
+//     // const sectionCategoryResponse = await fetch(
+//     //   `${process.env.API_URL}/api/section-categories?${sectionCategoryQuery}`,
+//     // );
 
-    const sectionBrandResponse = await fetch(
-      `${process.env.API_URL}/api/section-brands?${sectionBrandQuery}`,
-    );
-    // const footerDataJson = await footerResponse.json();
-    // const headerDataJson = await headerResponse.json();
-    const bannerDataJson = await bannerResponse?.json();
-    // const secondBannerDataJson = await secondBannerResponse?.json();
-    const popularCategoryDataJson = await popularCategoryResponse?.json();
-    // const newProductsJson = await newProductsResponse?.json();
-    // const clearenceDataJson = await clearenceResponse?.json();
-    // const sectionCategoryDataJson = await sectionCategoryResponse?.json();
-    const sectionBrandDataJson = await sectionBrandResponse?.json();
+//     const sectionBrandResponse = await fetch(
+//       `${process.env.API_URL}/api/section-brands?${sectionBrandQuery}`,
+//     );
+//     // const footerDataJson = await footerResponse.json();
+//     // const headerDataJson = await headerResponse.json();
+//     const bannerDataJson = await bannerResponse?.json();
+//     // const secondBannerDataJson = await secondBannerResponse?.json();
+//     const popularCategoryDataJson = await popularCategoryResponse?.json();
+//     // const newProductsJson = await newProductsResponse?.json();
+//     // const clearenceDataJson = await clearenceResponse?.json();
+//     // const sectionCategoryDataJson = await sectionCategoryResponse?.json();
+//     const sectionBrandDataJson = await sectionBrandResponse?.json();
 
-    return {
-      props: {
-        // footerData: footerDataJson,
-        // headerData: headerDataJson,
-        // newProductsData: newProductsJson?.data?.attributes?.sortedProducts,
-        // clearenceData: clearenceDataJson?.data,
-        // sectionCategoryData: sectionCategoryDataJson,
-        // secondBannerData: secondBannerDataJson?.data,
-        bannerData: bannerDataJson.data,
-        sectionPopularCategoryData: popularCategoryDataJson?.data,
-        sectionBrandData: sectionBrandDataJson,
-      },
-    };
-  } catch (error) {
-    return {
-      props: {
-        // footerData: null,
-        // headerData: null,
-        bannerData: null,
-        // newProductsData: null,
-        // clearenceData: null,
-        // sectionCategoryData: null,
-        // secondBannerData: null,
-        sectionPopularCategoryData: null,
-        sectionBrandData: null,
-      },
-    };
-  }
-}
+//     return {
+//       props: {
+//         // footerData: footerDataJson,
+//         // headerData: headerDataJson,
+//         // newProductsData: newProductsJson?.data?.attributes?.sortedProducts,
+//         // clearenceData: clearenceDataJson?.data,
+//         // sectionCategoryData: sectionCategoryDataJson,
+//         // secondBannerData: secondBannerDataJson?.data,
+//         bannerData: bannerDataJson.data,
+//         sectionPopularCategoryData: popularCategoryDataJson?.data,
+//         sectionBrandData: sectionBrandDataJson,
+//       },
+//     };
+//   } catch (error) {
+//     return {
+//       props: {
+//         // footerData: null,
+//         // headerData: null,
+//         bannerData: null,
+//         // newProductsData: null,
+//         // clearenceData: null,
+//         // sectionCategoryData: null,
+//         // secondBannerData: null,
+//         sectionPopularCategoryData: null,
+//         sectionBrandData: null,
+//       },
+//     };
+//   }
+// }
